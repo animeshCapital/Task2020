cb285140a66c2e0a2f56b8851bc7eebfca94f4d3
Soumyajit <soumyajit@capitalnumbers.com>
2020-03-26 18:08:24 +0530

    shouldQueue add in notification

--- app/Notifications/WelCome.php
+++ app/Notifications/WelCome.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class WelCome extends Notification
+class WelCome extends Notification implements ShouldQueue
 {
     use Queueable;
 
